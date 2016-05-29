0d7c52caced7eeb106706b01285fbc4b73ad98df
Justin Noah <justinnoah@gmail.com>
2016-05-28 19:37:35 -0700

    tare: page parser skeleton

---
+++ tare_page_parser.php
@@ -0,0 +1,91 @@
+<?php
+#  Copyright 2016 A Family For Every Child
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+/**
+ * Short Desc
+ *
+ * Child and Sibling Page Parser for TARE
+ */
+class PageParser
+{
+
+    /**
+     * Short Desc
+     *
+     * Parse all data needed from a child or sibling page
+     *
+     * @param string $url Child or Sibling Group url
+     *
+     * @return Child or SiblingGroup object
+     */
+    function parse($url)
+    {
+    }
+
+    /**
+     * Short Desc
+     *
+     * Parse Attachments for Child and Sibling Groups
+     */
+    function parse_attachments()
+    {
+    }
+
+    /**
+     * Short Desc
+     *
+     * Porse Caseworker Data and add it to the Child/Sibling object
+     */
+    function parse_caseworker_info()
+    {
+    }
+
+    /**
+    * Short Desc
+    *
+    * Parse child data from Child.aspx pages
+    */
+    function parse_child_info()
+    {
+    }
+
+    /**
+     * Short Desc
+     *
+     * Grab children from sibling group and parse them
+     */
+    function parse_children_in_group()
+    {
+    }
+
+    /**
+     * Short Desc
+     *
+     * Parse sibling group data from Group.aspx  pages
+     */
+    function parse_sibling_info()
+    {
+    }
+
+    /**
+     * Short Desc
+     *
+     * Identify which type of page to parse
+     */
+    function parse_info()
+    {
+    }
+}
+?>
