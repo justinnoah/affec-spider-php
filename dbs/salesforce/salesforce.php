36ef55a6aa409c1b8bf15a2758b07157c29178b6
Justin Noah <justinnoah@gmail.com>
2016-06-03 17:09:11 -0700

    sf: begin import from sf

--- dbs/salesforce/salesforce.php
+++ dbs/salesforce/salesforce.php
@@ -44,6 +44,7 @@ class Salesforce
         $this->sf_pass = $cfg["password"];
         $this->sf_token = $cfg["token"];
         $this->sf_sandbox = $cfg["sandbox"];
+        $this->sf_wsdl = $this->sf_sandbox ? "test" : "live";
         $this->cache_cfg = $cfg["cache_db"];
 
         // Setup logging
@@ -53,9 +54,51 @@ class Salesforce
 
         // Cache init
         $this->em = init_cache_db($this->cache_cfg);
+
+        // Import from SF
+        $this->import_from_sf();
     }
 
+    /**
+     * Pull from SF
+     */
+    function import_from_sf()
+    {
+        $this->log->debug("Logging Into SF");
+        $this->sf_conn = new \SforcePartnerClient();
+        $this->sf_conn->createConnection($this->sf_wsdl . ".partner.wsdl");
+        $this->sf_conn->login(
+            $this->sf_username, $this->sf_pass.$this->sf_token
+        );
+        $this->log->debug("Logged Into SF");
 
+        $this->import_sf_contacts();
+        $this->import_sf_children();
+        $this->import_sf_groups();
+        $this->import_sf_attachments();
+        $this->log->debug("Flushing SF Data");
+        $this->em->flush();
+    }
+
+    /**
+     * Query all the things
+     *
+     * @param string $query Query to call on sf
+     */
+    function sfQueryAll($query)
+    {
+        $all_records = array();
+        $response = $this->sf_conn->query($query);
+        array_push($all_records, $response->records);
+        $qr = new \QueryResult($response);
+        while (!$qr->done)
+        {
+            $response = $this->sf_conn->queryMore($qr->queryLocator);
+            array_push($all_records, $response->records);
+            $qr = new \QueryResult($response);
+        }
+        return $all_records;
+    }
 
     /**
      * Import Allchildren into our cache
@@ -78,6 +121,36 @@ class Salesforce
     }
 
     /**
+     * Import parsed contact as a cache object
+     *
+     * @param array $contact array to convert
+     */
+    protected function upsert_parsed_contact($contact)
+    {
+        $this->em->persist(CacheContact::from_parsed($contact));
+    }
+
+    /**
+     * Import parsed sf contact as a cache object
+     */
+    protected function import_sf_contacts()
+    {
+        $this->log->debug("SFImport: Contacts");
+        $select_fields = implode(", ", array_keys(CacheContact::sf_map));
+        $query = "SELECT Id, " . $select_fields . " FROM Contact WHERE MailingState='Texas'";
+        $all_records = $this->sfQueryAll($query);
+        foreach ($all_records as $records)
+        {
+            foreach ($records as $contact)
+            {
+                $this->em->persist(
+                    CacheContact::from_sf($contact->Id, $contact->fields)
+                );
+            }
+        }
+    }
+
+    /**
      * Import parsed child as a cache object
      *
      * @param array $child array to convert
@@ -89,12 +162,11 @@ class Salesforce
 
     /**
      * Import parsed child as a cache object
-     *
-     * @param \sObject $child array to convert
      */
-    protected function import_sf_child($child)
+    protected function import_sf_children()
     {
-        $this->em->persist(CacheChild::from_sf($child));
+        $this->log->debug("SFImport: Children");
+        // $this->em->persist(CacheChild::from_sf($child));
     }
 
     /**
@@ -109,32 +181,11 @@ class Salesforce
 
     /**
      * Import parsed sf group as a cache object
-     *
-     * @param \sObject $group array to convert
      */
-    protected function import_sf_group($group)
+    protected function import_sf_groups()
     {
-        $this->em->persist(CacheGroup::from_sf($group));
-    }
-
-    /**
-     * Import parsed contact as a cache object
-     *
-     * @param array $contact array to convert
-     */
-    protected function upsert_parsed_contact($contact)
-    {
-        $this->em->persist(CacheContact::from_parsed($contact));
-    }
-
-    /**
-     * Import parsed sf contact as a cache object
-     *
-     * @param array $contact array to convert
-     */
-    protected function import_sf_contact($contact)
-    {
-        $this->em->persist(CacheContact::from_sf($contact));
+        $this->log->debug("SFImport: Groups");
+        // $this->em->persist(CacheGroup::from_sf($group));
     }
 
     /**
@@ -149,12 +200,11 @@ class Salesforce
 
     /**
      * Import parsed attachment as a cache object
-     *
-     * @param array $attachment array to convert
      */
-    protected function import_sf_attachment($attachment)
+    protected function import_sf_attachments()
     {
-        $this->em->persist(CacheAttachment::from_parsed($attachment));
+        $this->log->debug("SFImport: Attachments");
+        // $this->em->persist(CacheAttachment::from_parsed($attachment));
     }
 
     /**
