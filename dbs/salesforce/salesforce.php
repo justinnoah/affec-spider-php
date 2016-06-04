7471fd041df278534fe42df03847036cdc9f502f
Justin Noah <justinnoah@gmail.com>
2016-06-04 14:31:12 -0700

    sf: rewrite much for DRY

--- dbs/salesforce/salesforce.php
+++ dbs/salesforce/salesforce.php
@@ -56,26 +56,34 @@ class Salesforce
         $this->em = init_cache_db($this->cache_cfg);
 
         // Import from SF
-        $this->import_from_sf();
+        $this->import_sf_into_cache();
     }
 
     /**
      * Pull from SF
      */
-    function import_from_sf()
+    function import_sf_into_cache()
     {
         $this->log->debug("Logging Into SF");
         $this->sf_conn = new \SforcePartnerClient();
         $this->sf_conn->createConnection($this->sf_wsdl . ".partner.wsdl");
         $this->sf_conn->login(
-            $this->sf_username, $this->sf_pass.$this->sf_token
+            $this->sf_username,
+            $this->sf_pass . $this->sf_token
         );
         $this->log->debug("Logged Into SF");
 
-        $this->import_sf_contacts();
-        $this->import_sf_children();
-        $this->import_sf_groups();
-        $this->import_sf_attachments();
+        // Grab Contacts
+        $where = "WHERE MailingState='" . CURRENT_STATE . "'";
+        $this->import_from_sf("CacheContact", $where);
+        // Grab Children
+        $where = "WHERE Child_s_State__c='" . CURRENT_STATE_LONG . "'";
+        $this->import_from_sf("CacheChild", $where);
+        // Grab Groups
+        $where = "WHERE State__c='" . CURRENT_STATE_LONG . "'";
+        $this->import_from_sf("CacheGroup", $where);
+        // Grab Attachments
+
         $this->log->debug("Flushing SF Data");
         $this->em->flush();
     }
@@ -87,17 +95,69 @@ class Salesforce
      */
     function sfQueryAll($query)
     {
-        $all_records = array();
+        $this->log->debug($query);
         $response = $this->sf_conn->query($query);
-        array_push($all_records, $response->records);
+        $all_records = array();
+        foreach ($response->records as $record)
+            array_push($all_records, $record);
         $qr = new \QueryResult($response);
         while (!$qr->done)
         {
             $response = $this->sf_conn->queryMore($qr->queryLocator);
-            array_push($all_records, $response->records);
+            foreach ($response->records as $record)
+                array_push($all_records, $record);
             $qr = new \QueryResult($response);
         }
         return $all_records;
+    }
+
+    /**
+     * Generic method to grab $type data from SF and import it into the cache
+     *
+     * @param string $type one of the four Cache types
+     * @param string $where WHERE clause for the SF query
+     */
+    protected function import_from_sf($type, $where)
+    {
+        $this->log->debug("SFImport: " . $type . "s");
+
+        // Simple type to SF Table name map
+        $type_table_map = array(
+            "CacheAttachment" => "Attachment",
+            "CacheChild" => "Children__c",
+            "CacheContact" => "Contact",
+            "CacheGroup" => "Sibling_Group__c"
+        );
+
+        // Build the field list needed for the data we track given $type
+        $fields = array_keys($type::sf_map);
+        array_push($fields, "Id");
+        $select_fields = implode(", ", $fields);
+
+        // Grab all the things from SF
+        $query = "SELECT $select_fields FROM $type_table_map[$type] $where";
+        $returned_records = $this->sfQueryAll($query);
+
+        // Either add new data or replace current data.
+        // SF store is the ultimate authority.
+        foreach ($returned_records as $sf_record)
+        {
+            // Using DQL] query our objects for existing ones
+            // http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/dql-doctrine-query-language.html
+            $exist = $this->em->createQuery(
+                "SELECT c FROM $type c WHERE c.sf_id = '$sf_record->Id'"
+            )->getResult();
+
+            // Add or replace
+            $new_cache_obj = $type::from_sf($sf_record->Id, $sf_record->fields);
+            if (!$exist)
+            {
+                $this->em->persist($new_cache_obj);
+            } else {
+                $this->em->remove($exist);
+                $this->em->persist($new_cache_obj);
+            }
+        }
     }
 
     /**
@@ -131,26 +191,6 @@ class Salesforce
     }
 
     /**
-     * Import parsed sf contact as a cache object
-     */
-    protected function import_sf_contacts()
-    {
-        $this->log->debug("SFImport: Contacts");
-        $select_fields = implode(", ", array_keys(CacheContact::sf_map));
-        $query = "SELECT Id, " . $select_fields . " FROM Contact WHERE MailingState='Texas'";
-        $all_records = $this->sfQueryAll($query);
-        foreach ($all_records as $records)
-        {
-            foreach ($records as $contact)
-            {
-                $this->em->persist(
-                    CacheContact::from_sf($contact->Id, $contact->fields)
-                );
-            }
-        }
-    }
-
-    /**
      * Import parsed child as a cache object
      *
      * @param array $child array to convert
@@ -158,15 +198,6 @@ class Salesforce
     protected function upsert_parsed_child($child)
     {
         $this->em->persist(CacheChild::from_parsed($child));
-    }
-
-    /**
-     * Import parsed child as a cache object
-     */
-    protected function import_sf_children()
-    {
-        $this->log->debug("SFImport: Children");
-        // $this->em->persist(CacheChild::from_sf($child));
     }
 
     /**
@@ -180,15 +211,6 @@ class Salesforce
     }
 
     /**
-     * Import parsed sf group as a cache object
-     */
-    protected function import_sf_groups()
-    {
-        $this->log->debug("SFImport: Groups");
-        // $this->em->persist(CacheGroup::from_sf($group));
-    }
-
-    /**
      * Import parsed attachment as a cache object
      *
      * @param array $attachment array to convert
@@ -196,15 +218,6 @@ class Salesforce
     protected function upsert_parsed_attachment($attachment)
     {
         $this->em->persist(CacheAttachment::from_parsed($attachment));
-    }
-
-    /**
-     * Import parsed attachment as a cache object
-     */
-    protected function import_sf_attachments()
-    {
-        $this->log->debug("SFImport: Attachments");
-        // $this->em->persist(CacheAttachment::from_parsed($attachment));
     }
 
     /**
